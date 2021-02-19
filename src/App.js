ebdcb0a7964e99664a04bf7020f3995a34ec4d9e
TehreemSultan <tehreemsultanali@gmail.com>
2021-02-19 20:14:04 +0500

    app and message component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import Message from './Message';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
 
 function App() {
+
+  const [input,setInput] = useState("");
+  const [username,setUsername] = useState("");
+  const [messages,setMessages] = useState([]);
+
+
+  useEffect(()=>{
+    setUsername(prompt('Please enter your name'));
+  },[]);
+
+  const sendMessage=(event)=>{
+    event.preventDefault();
+    setMessages([...messages, {username:username, text:input}]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Chat App</h1>
+      <h2>welcome {username}</h2>
+      <form>
+      <FormControl>
+      <InputLabel htmlFor="my-input">Enter a message.....</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value)}/>
+        <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
+      </FormControl>
+      </form>
+      {
+        messages.map(message=>(
+        <Message username={message.username} text={message.text}/>
+       ) )
+      }
     </div>
   );
 }
